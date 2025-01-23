5dbf99fe4553886ebd7dcad71c61334e9d12957f
Nate OConnell <oconrunner@gmail.com>
2025-01-23 14:46:55 -0500

    edit function

--- R/read_excel_allsheets.R
+++ R/read_excel_allsheets.R
@@ -9,7 +9,7 @@
 #' @return a list of dataframes or TIBBLEs corresponding to each Excel Sheet
 #' @export
 
-read.excel.allsheets <- function(filename, tibble = FALSE) {
+read_excel_allsheets <- function(filename, tibble = FALSE) {
   sheets <- readxl::excel_sheets(filename)
   x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
   if(!tibble) x <- lapply(x, as.data.frame)
