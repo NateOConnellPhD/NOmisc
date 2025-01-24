eccd10e682db03f2d21ac1a2768a2b63bf033dd6
Nate OConnell <oconrunner@gmail.com>
2025-01-24 10:06:28 -0500

    added char_to_factor function

---
+++ R/char_to_factor.R
@@ -0,0 +1,25 @@
+#' Converts all character variables to factor variables in a data frame
+#'
+#' When supplied a data frame with character variables, this function
+#' converts all character variables within a data frame to factor variables.
+#'
+#'
+#' @param df A data frame or tibble object.
+#' @return the same dataframe provided with all character variables converted to factor variables
+#' @export
+
+
+char_to_factor <- function(df) {
+  # Ensure the input is a data frame
+  if (!is.data.frame(df)) {
+    stop("Input must be a data frame")
+  }
+
+  # ISpecifiy character columns
+  char_cols <- sapply(df, is.character)
+
+  # convert character columns to factors
+  df[char_cols] <- lapply(df[char_cols], factor)
+
+  return(df)
+}
